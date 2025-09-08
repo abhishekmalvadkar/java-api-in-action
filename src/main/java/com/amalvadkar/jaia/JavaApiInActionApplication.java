c65f0363ed04af244dfcf8884693d4623d4bc63c
Abhishek <abhi@dev.com>
2025-09-08 12:51:14 +0530

    Initial commit

---
+++ src/main/java/com/amalvadkar/jaia/JavaApiInActionApplication.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.jaia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiInActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalvadkar/jaia/JavaApiInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.jaia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
