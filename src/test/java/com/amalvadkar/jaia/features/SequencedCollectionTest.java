a527f71540fa3170e0d3f336ce82e7cc17004095
Abhishek <abhi@dev.com>
2025-11-25 13:41:02 +0530

    should_return_list_in_reverse should_return_list_in_reverse_and_if_we_change_in_reversed_list_then_it_should_change_in_original_list

--- src/test/java/com/amalvadkar/jaia/features/SequencedCollectionTest.java
+++ src/test/java/com/amalvadkar/jaia/features/SequencedCollectionTest.java
@@ -4,6 +4,7 @@ import com.amalvadkar.jaia.common.AbstractFeatureUT;
 import com.amalvadkar.jaia.common.Feature;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.amalvadkar.jaia.common.FeatureName.SEQUENCED_COLLECTION;
@@ -20,4 +21,26 @@ public class SequencedCollectionTest extends AbstractFeatureUT {
         assertThat(employeeIds.getFirst()).isEqualTo(2);
         assertThat(employeeIds.getLast()).isEqualTo(3);
     }
+
+    @Test
+    void should_return_list_in_reverse() {
+        List<Integer> employeeIds = List.of(2, 5, 7, 3);
+
+        assertThat(employeeIds.reversed()).containsExactly(3, 7, 5, 2);
+    }
+
+    @Test
+    void should_return_list_in_reverse_and_if_we_change_in_reversed_list_then_it_should_change_in_original_list() {
+        List<Integer> employeeIds = new ArrayList<>(List.of(2, 5, 7, 3));
+
+        List<Integer> reversedList = employeeIds.reversed();
+        assertThat(reversedList).containsExactly(3, 7, 5, 2);
+
+        reversedList.add(8);
+        assertThat(employeeIds).containsExactly(8, 2, 5, 7, 3);
+    }
+
+
+
+
 }
