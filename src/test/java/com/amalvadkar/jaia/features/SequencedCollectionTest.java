1d59d2b76a63d08479b6199f7b591706c5f4aae6
Abhishek <abhi@dev.com>
2025-12-04 14:20:17 +0530

    should_return_list_in_reverse_and_if_we_change_in_reversed_list_then_it_should_throw_exception_if_my_original_list_is_immutable

--- src/test/java/com/amalvadkar/jaia/features/SequencedCollectionTest.java
+++ src/test/java/com/amalvadkar/jaia/features/SequencedCollectionTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import static com.amalvadkar.jaia.common.FeatureName.SEQUENCED_COLLECTION;
 import static com.amalvadkar.jaia.common.Version.JDK_21;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Feature(name = SEQUENCED_COLLECTION, version = JDK_21)
 public class SequencedCollectionTest extends AbstractFeatureUT {
@@ -38,6 +39,17 @@ public class SequencedCollectionTest extends AbstractFeatureUT {
 
         reversedList.add(8);
         assertThat(employeeIds).containsExactly(8, 2, 5, 7, 3);
+    }
+
+    @Test
+    void should_return_list_in_reverse_and_if_we_change_in_reversed_list_then_it_should_throw_exception_if_my_original_list_is_immutable() {
+        List<Integer> employeeIds = List.of(2, 5, 7, 3);
+
+        List<Integer> reversedList = employeeIds.reversed();
+        assertThat(reversedList).containsExactly(3, 7, 5, 2);
+
+        assertThatThrownBy(() -> reversedList.add(8))
+                .isInstanceOf(UnsupportedOperationException.class);
     }
 
 
